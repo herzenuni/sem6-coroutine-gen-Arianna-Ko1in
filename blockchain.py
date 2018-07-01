6b256c26851f084811d9b90b79aaa72d1cc7d2f3
Arianna-Ko1in <rishkolin@gmail.com>
2018-07-02 00:17:05 +0300

    Update blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -1,48 +1,30 @@
-import hashlib as hasher
-import datetime as date
-import string
+import random
+from hashlib import sha3_512
+import binascii
 
+genesis_block = bytearray([ 0 for i in range(0,64) ])
+baseblock = genesis_block
 
-class Block:
-    def __init__(self, index, timestamp, data, previous_hash):
-        """ Конструктор нового объекта """
-        self.index = index
-        self.timestamp = timestamp
-        self.data = data
-        self.previous_hash = previous_hash
-        self.hash = self.hash_block()
-        
-    def hash_block(self):
-        """ Функция для создания нового блока """
-        sha = hasher.sha256()
-        data = (str(self.index) + str(self.timestamp) + 
-                   str(self.data) + str(self.previous_hash)).encode('utf-8')
-        sha.update(data)
-        return sha.hexdigest()
-    
-    
-def create_genesis_block():
-    
-    return Block(0, date.datetime.now(), "This is first block data", str(0))
+def randomblock(limit):
+    for i in range(0,limit):
+        ml = sha3_512()
+        ml.update(str(random.randint(0,1e18)).encode("iso-8859-1"))
+        yield bytearray(ml.hexdigest().encode("iso-8859-1"))
 
+def signedblock(baseblock, limit):
+    for block in randomblock(limit):
+        block_data = baseblock + block
+        digest = sha3_512()
+        digest.update(block_data)
+        baseblock = digest.digest()
+        yield block_data + digest.digest()
 
-def next_block(last_block):
-    current_index = last_block.index + 1
-    
-    current_timestamp = date.datetime.now()
-    
-    current_data = str(current_index) + string.ascii_lowercase
-
-    current_hash = last_block.hash
-    
-    return Block(current_index, current_timestamp, current_data, current_hash)
-
-
-blockchain = [create_genesis_block()]
-
-number_of_blocks = 10
-
-for i in range(10):
-    new_block = next_block(blockchain[i])
-    blockchain.append(new_block)
-    print(f"Data: {new_block.data}, New block is : {new_block.hash} \n")
+limit = 30
+blocks = [ block for block in signedblock(genesis_block, limit) ]
+assert len(blocks)==limit
+for block in blocks:
+    print("link="+binascii.hexlify(block[0:64]).decode("iso-8859-1"))
+    print("data="+block[64:192].decode("iso-8859-1"))
+    print("digest="+binascii.hexlify(block[192:256]).decode("iso-8859-1"))
+    print("==========================")
+    assert(len(block)==256)         # 64 (reference) + 128 (data) + 64 (hash)
